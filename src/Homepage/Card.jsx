9e4199139a3dc5af5a6acd90848bb67184aca630
hammadawan424 <hammadawan9255@gmail.com>
2024-06-21 13:49:50 +0500

    Changed styles to tailwind

--- src/Homepage/Card.jsx
+++ src/Homepage/Card.jsx
@@ -1,4 +1,44 @@
-function Card({ title, text, btnText }) {
+import { useState } from "react";
+import { useRef } from "react";
+import { IconCross } from "@tabler/icons-react"
+import styles from "/src/Student/students.module.css"
+
+const types = {
+    create: {
+        title: "Create Class Group",
+        text: "Create a new class group to manage multiple classes on one screen",
+        btnText: "Create Now"
+    },
+    join: {
+        title: "Join Class Group",
+        text: "Join a class group as an admin to show others your class stats",
+        btnText: "Join Now"
+    },
+    tests: {
+        title: "Manage Tests",
+        text: "Manage marking of various tests and share them with your administration easily",
+        btnText: "Start Now"
+    }
+}
+
+function Card({type}) {
+
+    const [fieldVisible, setFieldVisible] = useState(false)
+    const dialogRef = useRef(null)
+    const { title, text, btnText } = types[type]
+
+    function closeDialog() {
+        dialogRef.current.close()
+    }
+
+    function openDialog() {
+        dialogRef.current.showModal()
+    }
+
+    // function tmp() {
+    //     setFieldVisible(true)
+    // }
+
     return (
         <div className="p-4 flex group overflow-hidden flex-col transition
             border-solid border-2 items-start rounded-md hover:bg-[--bg-hover-col]"
@@ -8,9 +48,35 @@ function Card({ title, text, btnText }) {
                 <div className="text-sm"> {text} </div>
             </div>
             <div className="flex-auto"></div>
-            <button>{btnText}</button>
+            <button onClick={openDialog}>{btnText}</button>
+            {/* <button onClick={tmp}>NOTHING</button> */}
+
+            <dialog id='submitDialog' ref={dialogRef}>
+                <div className={styles.submitDialog + " flex flex-col gap-2"}>
+                    <IconCross onClick={closeDialog} className="self-end" />
+                    <input required className="bg-stone-600 rounded-sm p-1" placeholder={btnText[0]} type="text" autoFocus />
+                    <div>
+                        <button autoFocus>Go back</button>
+                        <button>Yes submit</button>
+                    </div>
+                </div>
+
+            </dialog>
+
+            {/* {fieldVisible && <TmpComp />} */}
+
         </div>
     );
 }
+
+
+// function TmpComp() {
+//     return(
+//         <div className="opacity-50 bg-black absolute w-full h-screen">  
+//             <input type="text" />
+//         </div>
+//     )
+// }
+
 
 export default Card;
